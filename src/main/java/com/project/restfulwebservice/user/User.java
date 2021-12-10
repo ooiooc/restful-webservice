e66916d8a9c8fd25c0885e5b46985b091f2f11fa
ooiooc <shinjh1102@naver.com>
2021-12-10 23:18:54 +0900

    add: User Domain class

---
+++ src/main/java/com/project/restfulwebservice/user/User.java
@@ -0,0 +1,33 @@
+package com.project.restfulwebservice.user;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@JsonIgnoreProperties(value = {"password"})
+@JsonFilter("UserInfo")
+public class User {
+
+    private Integer id;
+
+    @Size(min=2, message = "name은 두 글자 이상 입력해주세요")
+    private String  name;
+
+    @Past
+    private Date joinDate;
+
+    private String password;
+
+    private String ssn;
+
+}
