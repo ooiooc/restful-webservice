1ae2470675a96f37cb47c369cff72a92de3410ad
ooiooc <shinjh1102@naver.com>
2021-12-27 03:38:58 +0900

    refactor : add post entity

--- src/main/java/com/project/restfulwebservice/user/User.java
+++ src/main/java/com/project/restfulwebservice/user/User.java
@@ -12,9 +12,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -43,4 +45,15 @@ public class User {
     @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요.")
     private String ssn;
 
+    @OneToMany(mappedBy = "user") // user 테이블의 데이터와 매핑될 수 있도록 설정
+    private List<Post> posts;
+
+
+    public User(int id, String name, Date joinDate, String password, String ssn) {
+        this.id=id;
+        this.name=name;
+        this.joinDate=joinDate;
+        this.password=password;
+        this.ssn=ssn;
+    }
 }
